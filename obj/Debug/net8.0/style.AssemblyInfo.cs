c90ffe28c73673cb4e6f9a25adbd9460c63fedbe
Julia V <v.yu.i.0460@gmail.com>
2025-03-11 13:59:00 +0700

    add readme

--- obj/Debug/net8.0/style.AssemblyInfo.cs
+++ obj/Debug/net8.0/style.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("style")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09eaeb962aa6e47730b4f25045fc370b00159281")]
 [assembly: System.Reflection.AssemblyProductAttribute("style")]
 [assembly: System.Reflection.AssemblyTitleAttribute("style")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
